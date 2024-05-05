057c1c357b737036d0351392581e9185885be1a3
Alejandro Martinez <alemar@Alejandros-MacBook-Pro.local>
2024-05-05 16:15:30 -0700

    Created the fetchApi function to retrieve data from the API.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
-
 const searchHistoryBtn = document.querySelector( ".search-history-btn" );
 const searchHistoryList = document.querySelector( ".search-history-list" );
+
+const apiObj = {
+  key: "873da18ac866fdce4b4c52d044835c91",
+  url: "http://api.openweathermap.org/geo/1.0/direct"
+};
+
+const fetchApi = function() {
+  fetch( `${ apiObj.url }?q=Norwalk,California,United States&appid=${ apiObj.key }` )
+    .then( function( res ) {
+      return res.json();
+    } )
+    .then( function( data ) {
+      console.log( data );
+    } );
+}
 
 searchHistoryBtn.addEventListener( "click", function() {
   const listHeight = searchHistoryList.scrollHeight;
@@ -12,3 +26,9 @@ searchHistoryBtn.addEventListener( "click", function() {
     searchHistoryList.dataset.state = "hidden";
   }
 } );
+
+const app = function() {
+  fetchApi();
+}
+
+app();
