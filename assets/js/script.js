6f9691752d7e6c9d13a4f63784ca48c3b89a089a
Alejandro Martinez <alemar@Alejandros-MacBook-Pro.local>
2024-05-08 16:52:01 -0700

    Created the createFormatDate and createFullForcast function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,77 @@
+"use strict";
+
+const searchBtn = document.querySelector( ".search-btn" );
+const searchInput = document.querySelector( ".search-input" );
 const searchHistoryBtn = document.querySelector( ".search-history-btn" );
 const searchHistoryList = document.querySelector( ".search-history-list" );
+const forcast = document.querySelector( ".forecast" );
 
 const apiObj = {
   key: "873da18ac866fdce4b4c52d044835c91",
-  url: "http://api.openweathermap.org/geo/1.0/direct"
+  geoCodeURL: "http://api.openweathermap.org/geo/1.0/direct",
+  weatherURL: "https://api.openweathermap.org/data/2.5/forecast",
+  currentWeatherURL: "https://api.openweathermap.org/data/2.5/weather"
 };
 
 const fetchApi = function() {
-  fetch( `${ apiObj.url }?q=Norwalk,California,United States&appid=${ apiObj.key }` )
+  return new Promise( function( resolve, reject ) {
+    const dataObj = {};
+    fetch( `${ apiObj.geoCodeURL }?q=${ searchInput.value }&appid=${ apiObj.key }` )
     .then( function( res ) {
       return res.json();
     } )
-    .then( function( data ) {
-      console.log( data );
-    } );
+    .then( function( geoCodeData ) {
+      const { lat, lon, country, name: city, state } = geoCodeData[ 0 ];
+      dataObj.city = city;
+      dataObj.state = state;
+      dataObj.country = country;
+      dataObj.lat = lat;
+      dataObj.lon = lon;
+      return fetch( `${ apiObj.currentWeatherURL }?lat=${ lat }&lon=${ lon }&appid=${ apiObj.key }&units=imperial` );
+    } )
+    .then( function( res ) {
+      return res.json();
+    } )
+    .then( function( currentWeatherData ) {
+      dataObj.weatherData = currentWeatherData;
+      return fetch( `${ apiObj.weatherURL }?lat=${ dataObj.lat }&lon=${ dataObj.lon }&appid=${ apiObj.key }` );
+    } )
+    .then( function( res ) {
+      return res.json();
+    } )
+    .then( function( forecastData ) {
+      console.log( forecastData.list );
+      resolve( dataObj );
+    } )
+  } );
+}
+
+// Creating a formatted date
+const createFormatDate = function( unformattedDate ) {
+  const date = new Date( unformattedDate );
+  const dateFormatter = new Intl.DateTimeFormat( "en-US", {
+    year : "numeric",
+    month : "2-digit",
+    day : "2-digit"
+  } );
+  return dateFormatter.format( date );
+}
+
+const createFullForcast = function( dataObj ) {
+  const { city, state, country, weatherData: { main : { humidity, temp }, wind : { speed }, weather } } = dataObj;
+  const weatherIcon = weather[ 0 ].icon;
+  const currentDate = createFormatDate( Date.now() );
+  let fullForcastHTML = 
+    `<div class="primary-forecast">
+      <div class="forecast-city-img-wrapper">
+        <h2 class="primary-forecast-city">${ city }, ${ state }, ${ country } ${ currentDate }</h2>
+        <img src="https://openweathermap.org/img/wn/${ weatherIcon }@2x.png" alt="Icon of the forecast" class="primary-forecast-img">
+      </div>
+      <p class="primary-forecast-temp">Temp: ${ temp }°F</p>
+      <p class="primary-forecast-wind">Wind: ${ speed } MPH</p>
+      <p class="primary-forecast-humidity">Humidity: ${ humidity }%</p>
+    </div>`;
+  forcast.innerHTML = fullForcastHTML;
 }
 
 searchHistoryBtn.addEventListener( "click", function() {
@@ -27,8 +85,11 @@ searchHistoryBtn.addEventListener( "click", function() {
   }
 } );
 
-const app = function() {
-  fetchApi();
-}
-
-app();
+searchBtn.addEventListener( "click", function( event ) {
+  event.preventDefault();
+  fetchApi()
+    .then( function( dataObj ) {
+      console.log( dataObj );
+      createFullForcast( dataObj );
+    } );
+} );
